9f2c64285aa5f2be8ad020c24dc9d2286e17345e
Denis <dekkedenis@mail.ru>
2024-02-18 17:10:36 +0300

    init

---
+++ MyThreads/MyThreadPool.cs
@@ -0,0 +1,98 @@
+﻿namespace MyThreads;
+
+public sealed class MyThreadPool : IDisposable
+{
+    private readonly Thread[] _threads;
+    private readonly Queue<Action> _actions;
+    private readonly object _syncObject = new();
+    private bool IsDisposed { get; set; }
+
+    public MyThreadPool(int maxThreads = 4)
+    {
+        _threads = new Thread[maxThreads];
+        _actions = new Queue<Action>();
+        for (var i = 0; i < _threads.Length; i++)
+        {
+            _threads[i] = new Thread(ThreadProc)
+            {
+                IsBackground = true,
+                Name = $"MyThreadPool Thread {i}"
+            };
+            _threads[i].Start();
+        }
+    }
+
+    private void ThreadProc()
+    {
+        while (true)
+        {
+            Action action;
+            Monitor.Enter(_syncObject);
+            try
+            {
+                if (IsDisposed)
+                {
+                    return;
+                }
+                if (_actions.Count > 0)
+                {
+                    action = _actions.Dequeue();
+                }
+                else
+                {
+                    Monitor.Wait(_syncObject);
+                    continue;
+                }
+            }
+            finally
+            {
+                Monitor.Exit(_syncObject);
+            }
+            action.Invoke();
+        }
+    }
+
+
+    public void Queue(Action action)
+    {
+        Monitor.Enter(_syncObject);
+        try
+        {
+            _actions.Enqueue(action);
+            if (_actions.Any())
+            {
+                Monitor.Pulse(_syncObject);
+            }
+        }
+        finally
+        {
+            Monitor.Exit(_syncObject);
+        }
+    }
+
+    public void Dispose()
+    {
+        if (IsDisposed) return;
+        var isDisposing = false;
+        Monitor.Enter(_syncObject);
+        try
+        {
+            if (!IsDisposed)
+            {
+                IsDisposed = true;
+                Monitor.PulseAll(_syncObject);
+                isDisposing = true;
+            }
+        }
+        finally
+        {
+            Monitor.Exit(_syncObject);
+        }
+
+        if (!isDisposing) return;
+        foreach (var t in _threads)
+        {
+            t.Join();
+        }
+    }
+}
---
+++ MyThreadsExample/Program.cs
@@ -0,0 +1,14 @@
+﻿using MyThreads;
+
+using (new Mutex(true, "MyMutextPrivet", out var mutex))
+{
+    if (!mutex) return;
+
+    using (var pool = new MyThreadPool())
+    {
+        pool.Queue(() => Console.WriteLine("From ThreadPool"));
+    }
+
+    Console.ReadKey();
+}
+
